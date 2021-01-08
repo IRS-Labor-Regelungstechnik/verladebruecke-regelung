3efa87ce972b1080f17d43b4269557cac50c7451
Nicolai Tschuch <nicolai.tschuch@web.de>
2021-01-08 10:12:29 +0100

    - Analyseskript für Messungen kommentiert

--- Andys Helper Functions/slice_time_series.m
+++ Andys Helper Functions/slice_time_series.m
@@ -1,12 +1,14 @@
 function ts = slice_time_series(ts_in, start_time, end_time)
-    % ts_in must have size 2xN with first row being time stamps
-    start_index = find(ts_in(1,:)>start_time, 1);
-    end_index = find(ts_in(1,:)>end_time, 1);
-    if isempty(start_index)
-        start_index = 1;
-    end 
-    if isempty(end_index)
-        end_index = size(ts_in, 2);
-    end 
-    ts = ts_in(:,start_index:end_index);
+% schneide bestimmte Bereich aus ts_in aus
+% ts_in must have size 2xN with first row being time stamps
+
+start_index = find(ts_in(1,:)>start_time, 1);
+end_index = find(ts_in(1,:)>end_time, 1);
+if isempty(start_index)
+    start_index = 1;
+end
+if isempty(end_index)
+    end_index = size(ts_in, 2);
+end
+ts = ts_in(:,start_index:end_index);
 end
--- Messungen_02_12_2020/exp_mess_analyse.m
+++ Messungen_02_12_2020/exp_mess_analyse.m
@@ -1,21 +1,40 @@
+%
+% erzeuge Plot der 4 Messungen
+%
+
+%%
+
+% _1: Messung des Signals des Absolutwertgebers
+% _2: Messung des Greiferwinkels
+% _3: Messung des Eingangssignals
+% _4: Messung des Resolversignals
+
+%%
 clear all;
 
+%% Parameter
 start_zeit = 13;
 end_zeit = 1000;
 
 measurement_nr = '7';
 nr_files = 4;
 
-eliminate_rollover_for = 4;
+eliminate_rollover_for = 4; % Messung des Resolvers -> eliminiere rollover
 
+%% Plotte Messungen
 for i = 1:nr_files
+    % lade Daten der aktuellen Messung
     file_name = strcat('mess',measurement_nr, '_', num2str(i));
     s = load(strcat(file_name, '.mat'));
     input_sig = [s.(file_name).X.Data; s.(file_name).Y.Data];
+    
+    % eliminiere rollover
     if any(eliminate_rollover_for == i)
         input_sig(2,:) = eliminate_rollover(input_sig(2,:));
     end
-    input_sig = slice_time_series(input_sig, start_zeit, end_zeit);
+    
+    % plot
+    input_sig = slice_time_series(input_sig, start_zeit, end_zeit); % schneide Bereich aus
     subplot(nr_files,1,i);
     plot(input_sig(1,:), input_sig(2,:))
 end
