d3554ef8b665b56b154ffdb37be5334529f26e5f
Andrew DeLay <signup@andrewdelay.com>
2021-01-13 14:18:18 +0100

    Letzte Aenderungen eingepflegt

--- Simulink/Modellierung/Modulares_Modell/StartSim.m
+++ Simulink/Modellierung/Modulares_Modell/StartSim.m
@@ -10,6 +10,7 @@ r1 = 0.05;
 r2 = 0.05;
 ue1 = 6;
 ue2 = 3;
+st1 = 0.1;
 d = 0.02;  % Daempfung
 
 %% Input
@@ -27,12 +28,10 @@ assignin('base', 't2',8)
 %% Nonlinear Model
 
 assignin('base', 'T1',T1)
-assignin('base', 'K1',K1)
 assignin('base', 'r1',r1)
 assignin('base', 'ue1',ue1)
 
 assignin('base', 'T2',T2)
-assignin('base', 'K2',K2)
 assignin('base', 'r2',r2)
 assignin('base', 'ue2',ue2)
 
@@ -62,48 +61,6 @@ D1=[0 0 0 0]';
 
 %Hubmotor
 A2=[0 1 ; 0 -1/T2];
-B2=[0  2*pi*r2*K2/(ue2*T2)]';
+B2=[0  2*pi*r2/(ue2*T2)]';
 C2=eye(2);
 D2=[0 0]';
-
-%% run the simulink model
-
-sim modular_NL_Modell
-%sim linearesModell
-
-%% plot
-
-figure(1)
-
-subplot(8,1,1)
-plot(tout,u(:,1))
-ylabel('u_{1}=n_{weg}')
-
-subplot(8,1,2)
-plot(tout,u(:,2))
-ylabel('u_{1}=n_{hub}')
-
-subplot(8,1,3)
-plot(tout,x(:,1))
-ylabel('x_{1}=x')
-
-subplot(8,1,4)
-plot(tout,x(:,2))
-ylabel('x_{2}=x^?')
-
-subplot(8,1,5)
-plot(tout,x(:,3))
-ylabel('x_{3}=L')
-
-subplot(8,1,6)
-plot(tout,x(:,4))
-ylabel('x_{4}=L^?')
-
-subplot(8,1,7)
-plot(tout,x(:,5))
-ylabel('x_6=phi')
-
-subplot(8,1,8)
-plot(tout,x(:,6))
-ylabel('x_5=phi^?')
-xlabel('t [s]')
