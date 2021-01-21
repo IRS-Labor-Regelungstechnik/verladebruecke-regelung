9729be594c361aaa86523ec3f2f38fa07d774144
Andrew DeLay <signup@andrewdelay.com>
2021-01-21 16:23:55 +0100

    trommelradius.m nicht gebraucht

--- Messungen/trommel_radius.m
+++
@@ -1,7 +0,0 @@
-function r = trommel_radius(L)
-    L_max = 160.5; %cm
-    L_min = 23; %cm
-    r_1 = 2.35; %cm
-    r_0 = 1.85;  %cm
-    r = ((r_0 - r_1) / (L_max - L_min)) * (L - L_min) + r_1;
-end
