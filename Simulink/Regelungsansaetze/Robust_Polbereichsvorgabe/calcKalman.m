13ce1c7eea0854fa7b1de1ddedbaf3814639fca5
Nicolai Tschuch <nicolai.tschuch@web.de>
2021-02-08 11:07:30 +0100

    - kleinere Änderungen - Offsetabgleich hinzugefügt

--- Simulink/Regelungsansaetze/Robust_Polbereichsvorgabe/calcKalman.m
+++ Simulink/Regelungsansaetze/Robust_Polbereichsvorgabe/calcKalman.m
@@ -16,12 +16,9 @@ sys = system;
 
 % Kovarianz
 matR_ges = diag([1; 1]); % Kovarianzmatrix Ausgangsrauschen
-matQ_ges = diag([5; 15]); % Kovarianzmatrix Systemrauschen
+matQ_ges = diag([1; 15]); % Kovarianzmatrix Systemrauschen
 
 % Systemrauschen
-% Annahme: wird bei der Übertragung der Solldrehzahl von der dSpace Box zum
-% Umrichter in die Eingangsgröße eingekoppelt, in die Störgrößen wird dabei
-% ebenfalls ein Rauschen eingekoppelt
 [~, matB_ges, ~, ~, ~, ~, ~] = calcSysMatrixStoer(sys);
 matG_ges = [matB_ges; matB_ges(2); 0]; % Spalte 1
 matG_ges = [matG_ges, [0; 0; 0; matB_ges(4); 0; 1]]; % Spalte 2
