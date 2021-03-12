a69482ce5a4c5014d639c5d5d74cfa159a6f7a6d
Andrew DeLay <signup@andrewdelay.com>
2021-03-12 13:18:04 +0100

    Messungs-Plots verbessert

--- Messungen/plot_P_regler_automatisierung.m
+++ Messungen/plot_P_regler_automatisierung.m
@@ -4,6 +4,8 @@ data = data.P_Regler_mit_Butter;
 LEG_FONT_SIZE = 10;
 LABEL_FONT_SIZE = 12;
 
+linienstaerke = 1;
+
 t = data.X.Data;
 
 relevant_data = ...
@@ -23,12 +25,33 @@ relevant_data = ...
 ylab = ['\vartheta', '\dot \vartheta', '\dot y_1', 'Magnet', '\dot y_2', 'Sollwert y_1', 'Sollwert y_2'];
 legends = {'Winkelausschlag', 'Winkelgeschw', 'Laufkgeschw', 'Magnet', 'Hubmotorgeschw', 'Sollwert y_1', 'Sollwert y_2'};
 
+
 %% X Soll und Pos
 figure(1)
 %plot(t, data.Y(15).Data, t, data.Y(8).Data, t, 0.995*data.Y(10).Data, t, data.Y(11).Data);
 %leg = legend('Sollwert $y_1$', 'Istwert $y_1$', 'Magnet', 'Regler aktiv');
-plot(t, data.Y(15).Data, t, data.Y(8).Data, t, 0.995*data.Y(10).Data);
-leg = legend('Sollwert $y_1$', 'Istwert $y_1$', 'Magnet');
+
+rect_start = -1;
+rect_on = false;
+for i = 1:length(data.Y(10).Data)
+    if data.Y(10).Data(i) == 1 && ~rect_on
+        rect_start = i;
+        rect_on = true;
+    end
+    if data.Y(10).Data(i) == 0 && rect_on
+        rectangle('Position',[t(rect_start),0.01,t(i-rect_start),3],'FaceColor',[1, 1, 0.7], 'LineWidth', 1, 'EdgeColor', 'none')
+        rect_on = false;
+    end
+end
+
+
+hold on
+plot(t, data.Y(15).Data .* 2.54, t, data.Y(8).Data .* 2.54); %, t, 0.995*data.Y(10).Data);
+
+xlim([0, 320]);
+ylim([0, 2.54]);
+
+leg = legend('Sollposition', 'Istposition'); %, 'Magnet');
 leg.Interpreter = 'latex';
 leg.Location = 'south';
 leg.FontSize = LEG_FONT_SIZE;
@@ -38,7 +61,7 @@ lab = xlabel('$t$ [s]');
 lab.FontSize = LABEL_FONT_SIZE;
 lab.Interpreter = 'latex';
 
-lab = ylabel('[V]');
+lab = ylabel('[m]');
 lab.Interpreter = 'latex';
 lab.FontSize = LABEL_FONT_SIZE;
 
@@ -49,8 +72,26 @@ figure(2)
 ax=axes; 
 % plot(t, data.Y(18).Data, t, data.Y(12).Data, t, 0.995*data.Y(10).Data, t, data.Y(11).Data);
 % leg = legend('Sollwert $y_2$', 'Istwert $y_2$', 'Magnet', 'Regler aktiv');
-plot(t, data.Y(18).Data, t, data.Y(12).Data, t, 0.995*data.Y(10).Data);
-leg = legend('Sollwert $y_2$', 'Istwert $y_2$', 'Magnet');
+
+rect_start = -1;
+rect_on = false;
+for i = 1:length(data.Y(10).Data)
+    if data.Y(10).Data(i) == 1 && ~rect_on
+        rect_start = i;
+        rect_on = true;
+    end
+    if data.Y(10).Data(i) == 0 && rect_on
+        rectangle('Position',[t(rect_start),0.01,t(i-rect_start),2],'FaceColor',[1, 1, 0.7], 'LineWidth', 1, 'EdgeColor', 'none')
+        rect_on = false;
+    end
+end
+
+hold on
+plot(t, data.Y(18).Data .* 1.37 + 0.23, t, data.Y(12).Data .* 1.37 + 0.23); %,t , 0.995*data.Y(10).Data);
+ylim([0, 1.7])
+xlim([0, 320]);
+
+leg = legend('Sollposition', 'Istposition'); %, 'Magnet');
 leg.Interpreter = 'latex';
 leg.Location = 'south';
 leg.FontSize = LEG_FONT_SIZE;
@@ -60,14 +101,9 @@ lab = xlabel('$t$ [s]');
 lab.Interpreter = 'latex';
 lab.FontSize = LABEL_FONT_SIZE;
 
-lab = ylabel('[V]');
+lab = ylabel('[m]');
 lab.Interpreter = 'latex';
 lab.FontSize = LABEL_FONT_SIZE;
 
 saveas(gcf,'P_regler_autom_Y.eps','epsc')
 
-%% Winkel
-%figure(3)
-%plot(t, data.Y(4).Data,  t, 10* data.Y(9).Data,  t, 10* data.Y(13).Data);
-%leg = legend('Winkelausschlag $y_3$', 'Laufkatzengeschw. $\dot y_1$', 'Hubmotorgeschw. $\dot y_2$');
-%leg.Interpreter = 'latex';
